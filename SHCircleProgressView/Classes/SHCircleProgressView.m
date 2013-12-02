986739222c9d096fdaa20b1f41ea0f1c67669778
ShengHuaWu <fantasy0404@gmail.com>
2013-12-02 15:54:57 +0800

    Modify SHCircleProgressView to smooth the animation

--- SHCircleProgressView/Classes/SHCircleProgressView.m
+++ SHCircleProgressView/Classes/SHCircleProgressView.m
@@ -8,18 +8,36 @@
 
 #import "SHCircleProgressView.h"
 
+/**
+ *  This string is an attribute, which you can apply to the user info in a timer.
+ *  The value of this attribute is an NSNumber object containing an boolean.
+ *  The value indicates whether the circle progress view repeats animating.
+ */
+static NSString *const SHCircleProgressViewRepeatAnimatingKey = @"SHCircleProgressViewRepeatAnimatingKey";
+
 @interface SHCircleProgressView ()
+@property (nonatomic, assign) CGFloat lastProgress;
 @property (nonatomic, strong) NSTimer *timer;
 @property (nonatomic, weak, readwrite) UILabel *titleLabel;
 @end
 
 @implementation SHCircleProgressView
 
+- (void)setLastProgress:(CGFloat)lastProgress
+{
+    if (_lastProgress != lastProgress) {
+        _lastProgress = lastProgress;
+        [self setNeedsDisplay];
+    }
+}
+
 - (void)setProgress:(CGFloat)progress
 {
     if (_progress != progress) {
         _progress = progress;
-        [self setNeedsDisplay];
+        if (!self.timer) {
+            self.timer = [NSTimer scheduledTimerWithTimeInterval:0.0625f target:self selector:@selector(updateLastProgress:) userInfo:@{SHCircleProgressViewRepeatAnimatingKey: [NSNumber numberWithBool:NO]} repeats:YES];
+        }
     }
 }
 
@@ -28,7 +46,6 @@
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor clearColor];
-        _progress = 0.0f;
         _circleGap = 5.0f;
         _radius = 40.0f;
         _maskColor = [UIColor colorWithWhite:0.9f alpha:0.7f];
@@ -82,7 +99,7 @@
          *        1/2 pi
          *
          */
-        CGFloat startAngle = self.progress * 2.0 * M_PI - M_PI_2; // Map the progress to the angle
+        CGFloat startAngle = self.lastProgress * 2.0 * M_PI - M_PI_2; // Map the progress to the angle
         CGPathAddArc(path, NULL, self.center.x, self.center.y - (CGRectGetHeight(self.titleLabel.frame) + 5.0f) * 0.5f, self.radius - self.circleGap, startAngle, 3.0 * M_PI_2, NO);
         CGContextAddPath(context, path);
         CGContextClosePath(context);
@@ -98,7 +115,7 @@
 - (void)startAnimating
 {
     if (!self.timer) {
-        self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5f target:self selector:@selector(updateProgress:) userInfo:nil repeats:YES];
+        self.timer = [NSTimer scheduledTimerWithTimeInterval:0.0625f target:self selector:@selector(updateLastProgress:) userInfo:@{SHCircleProgressViewRepeatAnimatingKey: [NSNumber numberWithBool:YES]} repeats:YES];
     }
 }
 
@@ -111,12 +128,20 @@
 }
 
 #pragma mark - Timer selector
-- (void)updateProgress:(NSTimer *)timer
+- (void)updateLastProgress:(NSTimer *)timer
 {
-    if (self.progress < 1.0f) {
-        self.progress += 0.05f;
+    NSDictionary *userInfo = [timer userInfo];
+    CGFloat maxProgress = [userInfo[SHCircleProgressViewRepeatAnimatingKey] boolValue] ? 1.0f : self.progress;
+    if (self.lastProgress + 0.0125f < maxProgress) {
+        self.lastProgress += 0.0125f;
     } else {
-        self.progress = 0.0f;
+        if ([userInfo[SHCircleProgressViewRepeatAnimatingKey] boolValue]) {
+            self.lastProgress = 0.0f;
+        } else {
+            self.lastProgress = self.progress;
+            [self.timer invalidate];
+            self.timer = nil;
+        }
     }
 }
 
